88d14e143816a8cabb90a97de8bf27c167582491
francisco.trabol1 <francisco.trabol@i-car.cl>
2024-06-06 10:40:16 -0400

    delete model harvest and clients

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,12 @@ import { config } from "dotenv";
 config();
 import environments from "./shared/environments";
 import app from "./shared/app";
-import ClientRouter from "./modules/clients/infrastructure/api/routes/client.router";
-import HarvestRouter from "./modules/harvests/infrastructure/api/routes/harvest.router";
+import ReportRouter from "./modules/report/infrastructure/api/routes/report.router";
 async function init() {
   const version:string = environments.APP_VERSION;
   //correr routers
   await new app([
-    new ClientRouter(version),
-    new HarvestRouter(version),
+    new ReportRouter(version),
   ], environments.APP_PORT).listen();
 }
 init();
